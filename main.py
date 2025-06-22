5f1d7a62542105999aa57159e3a65a466d993229
Taviquenson <tavo2070@gmail.com>
2025-06-22 10:31:11 -0700

    add input

---
+++ main.py
@@ -0,0 +1,25 @@
+import os, sys
+from dotenv import load_dotenv
+
+if len(sys.argv) < 2:
+    print("Error, no prompt was provided after program name")
+    sys.exit(1)
+
+load_dotenv()
+api_key = os.environ.get("GEMINI_API_KEY")
+
+from google import genai
+
+# create new instance of Gemini AI client
+client = genai.Client(api_key=api_key)
+
+response = client.models.generate_content(
+    model='gemini-2.0-flash-001', contents=sys.argv[1]
+)
+print(response.text)
+
+prompt_tokens = response.usage_metadata.prompt_token_count
+response_tokens = response.usage_metadata.candidates_token_count
+
+print(f"Prompt tokens: {prompt_tokens}")
+print(f"Response tokens: {response_tokens}")
