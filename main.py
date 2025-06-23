dd7e0b7099be725787b5c9e99884a8ae9585864f
Tavo <tavo@Tavos-MacBook-Pro.local>
2025-06-23 10:08:34 -0700

    add messages & verbose

--- main.py
+++ main.py
@@ -9,17 +9,28 @@ load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
 
 from google import genai
+from google.genai import types
+
+user_prompt = sys.argv[1]
+
+messages = [
+    types.Content(role="user", parts=[types.Part(text=user_prompt)]),
+]
 
 # create new instance of Gemini AI client
 client = genai.Client(api_key=api_key)
 
 response = client.models.generate_content(
-    model='gemini-2.0-flash-001', contents=sys.argv[1]
+    model='gemini-2.0-flash-001', contents=messages
 )
 print(response.text)
 
 prompt_tokens = response.usage_metadata.prompt_token_count
 response_tokens = response.usage_metadata.candidates_token_count
 
-print(f"Prompt tokens: {prompt_tokens}")
-print(f"Response tokens: {response_tokens}")
+if len(sys.argv) == 3 and sys.argv[2] == "--verbose":
+    print(f"User prompt: {user_prompt}")
+    print(f"Prompt tokens: {prompt_tokens}")
+    print(f"Response tokens: {response_tokens}")
+
+
